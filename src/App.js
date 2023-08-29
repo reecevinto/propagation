452e4d6f8067f712ba9fb0a58eca62bfc1f63b22
Collins Murgor <vintoncollins702@gmail.com>
2023-08-29 14:10:21 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function Toolbar() {
+  return (
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <button onClick={() => alert("Playing!")}>Play Movie</button>
+      <button onClick={() => alert("Uploading!")}>Upload Image</button>
+    </div>
+  );
+}
